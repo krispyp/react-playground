542e713697abb39f50edc7395255d35ed4ee6067
krispyp <chrisplastow@yahoo.co.uk>
2025-10-30 10:08:56 +0000

    feat: add Header component and use it in App

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+
+    return (
+        <header style={{padding: "1rem 0"}}>
+            <h1 style={{margin: 0}}>React Playground Tester</h1>
+            <p style={{margin: 0}}>Learning Git + VS Code like a pro</p>
+        </header>
+    );
+
+}
